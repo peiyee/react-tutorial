eed1c62249c503ffc6755bd26693ab4e8df853ed
peiyee <tehpeiyee_1989@hotmail.com>
2016-03-22 18:24:49 +0800

    reach tutorial 10, get input from json

--- public/scripts/my_try.js
+++ public/scripts/my_try.js
@@ -3,7 +3,7 @@ var CommentBox = React.createClass({
     return(
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList/>
+        <CommentList data={this.props.data}/>
         <CommentForm/>
       </div>
     );
@@ -21,15 +21,38 @@ var CommentForm = React.createClass({
 });
 
 var CommentList = React.createClass({
+
   render: function() {
+    var commentNodes = this.props.data.map(function(comment){
+      return(
+          <Comment author={comment.author} key={comment.id}>{comment.text}</Comment>
+        );
+    });
     return(
     <div className="commentList">
-      hello, iam a comment list
+      {commentNodes}
     </div>
     );
   }
 });
 
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(),{sanitize: true});
+    return {__html: rawMarkup};
+  },
+  render: function() {
+   return(
+    <div className="comment">
+      <h2 className="commentAuthor">{this.props.author}</h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+        </div>
+    );
+  }
+});
 
-
-ReactDOM.render(<CommentBox/>,document.getElementById("content"));
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+ReactDOM.render(<CommentBox data={data}/>,document.getElementById("content"));
