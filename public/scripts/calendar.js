67c81c659a8d5c97b3e5ecaeb71cba32fc56b3da
peiyee <tehpeiyee_1989@hotmail.com>
2016-03-24 23:26:26 +0800

    added dropdown to input day, open and close

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -107,9 +107,11 @@ var WorkForm = React.createClass({
 		this.setState({id: newProps.isEdit.id});
 	},
 	handleOpenChange: function(e){
+		console.log("typed");
 		this.setState({open: e.target.value});
 	},
 	handleDayChange: function(e){
+		console.log("typed");
 		this.setState({day: e.target.value});
 	},
 	handleCloseChange: function(e){
@@ -138,33 +140,63 @@ var WorkForm = React.createClass({
 		this.setState({day: "",open: "",close:"",id:""});
 	},
 	componentDidMount: function(){
-      this.refs.dayinput.focus(); 
+		console.log("Did mount");
+      $('div[name="dayInputDropdown"]')
+        .dropdown({
+        	onChange: (value,text) =>{
+        		console.log(value);
+        		this.setState({day: value});
+        	}
+        });
+      $('div[name="openInputDropdown"]')
+        .dropdown({
+        	onChange: (value,text) =>{
+        		console.log(value);
+        		this.setState({open: value});
+        	}
+        });
+       $('div[name="closeInputDropdown"]')
+          .dropdown({
+          	onChange: (value,text) =>{
+          		console.log(value);
+          		this.setState({close: value});
+          	}
+          });
     },
+    componentDidUpdate() {
+    	console.log("Did update");
+        $('.ui.dropdown').dropdown('refresh');
+     },
 	render: function(){
 		return(
 			<form onSubmit={this.props.isEdit ? this.handleUpdateWork : this.handleSubmitWork} className="ui form">
 			    <div className="four fields">
 			      <div className="field">
 			        <label>Day</label>
-			        <input 
-			         ref="dayinput"
-			         type="text" 
-			         onChange={this.handleDayChange} 
-			         value={this.state.day}/>
+			        <div name="dayInputDropdown" className="ui fluid search selection dropdown">
+			          <input type="hidden" name="user"/>
+			          <i className="dropdown icon"></i>
+			          <div ref="dayInput" onChange={this.handleDayChange} className="default text">Select</div>
+			          <DayList days={days}/>
+			        </div>
 			      </div>
 			      <div className="field">
-							    <label>Open</label>
-							    <input 
-			         type="text" 
-			         onChange={this.handleOpenChange} 
-			         value={this.state.open}/>
+				    <label>Open</label>
+				    <div name="openInputDropdown" className="ui fluid search selection dropdown">
+			          <input type="hidden" name="user"/>
+			          <i className="dropdown icon"></i>
+			          <div onChange={this.handleOpenChange} className="default text">Select</div>
+			          <TimeList timeSlots={timeSlots}/>
+			        </div>
 			      </div>
 			      <div className="field">
-							   <label>close</label>
-							   <input 
-			        type="text" 
-			        onChange={this.handleCloseChange} 
-			        value={this.state.close}/>
+				    <label>Close</label>
+				    <div name="closeInputDropdown" className="ui fluid search selection dropdown">
+			          <input type="hidden" name="user"/>
+			          <i className="dropdown icon"></i>
+			          <div onChange={this.handleCloseChange} className="default text">Select</div>
+			          <TimeList timeSlots={timeSlots}/>
+			        </div>
 			      </div>
 			    </div>
 				<input className="ui button" type="submit" value={this.props.isEdit ? "Update":"Save"}/>
@@ -172,5 +204,101 @@ var WorkForm = React.createClass({
 			);
 	}
 });
+
+var DayList = React.createClass({
+	render: function(){
+		var days = this.props.days.map(function(entry){
+				return(
+					  <div key={_.uniqueId()} className="item">
+					    {entry.full}
+					  </div>
+					  );
+				},this);
+		return(
+				<div className="menu">
+					{days}
+				</div>
+				);
+	}
+
+});
+
+var TimeList = React.createClass({
+	render: function(){
+		var timeSlots = this.props.timeSlots.map(function(entry){
+			return(
+					<div key={_.uniqueId()} className="item">
+						{entry}
+					</div>
+				);
+		},this);
+		return(
+				<div className="menu">
+					{timeSlots}
+				</div>
+			);
+	}
+
+});
+
+var getTimeSlot = function(divider){
+	var append = function(e,elem){
+		return elem+e;
+	};
+	var total_minutes = 12 * 60;
+	divider = divider || 60;
+	var slots = [total_minutes];
+	while(total_minutes>0){
+		total_minutes -= divider;
+		slots.push(total_minutes);
+	}
+	slots.pop();
+	slots = slots.reverse().map(convertMinutesToHourMinute);
+	slots = slots.map(append.bind(null,"am")).concat(slots.map(append.bind(null,"pm")));
+	return slots;
+};
+
+var convertMinutesToHourMinute = function(min){
+	if(min<0)
+		return;
+	var hours = (Math.trunc(min/60)).toString();
+	var minutes = (min % 60).toString();
+	minutes = minutes.length==2 ? minutes : minutes+"0";
+	return(hours+":"+minutes);
+};
+
+var timeSlots = getTimeSlot(60);
+
+var days = [
+  {
+    "full": "Monday",
+    "short":"Mon"
+  },
+  {
+    "full": "Tuesday",
+    "short":"Tue"
+  },
+  {
+    "full": "Wednesday",
+    "short":"Wed"
+  },
+  {
+    "full": "Thursday",
+    "short":"Thu"
+  },
+  {
+    "full": "Friday",
+    "short":"Fri"
+  },
+  {
+    "full": "Saturday",
+    "short":"Sat"
+  },
+  {
+    "full": "Sunday",
+    "short":"Sun"
+  }
+];
 //{this.props.isEdit ? "Update":"Save"}
+// ReactDOM.render(<WorkBox/>,document.getElementById('content'));
 ReactDOM.render(<WorkBox/>,document.getElementById('content'));
